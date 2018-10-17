7f0c03c6655600a6fbb57a6532aac14fdc53e6bc
thanhtungdp <tungptkh@gmail.com>
2018-10-17 16:48:35 +0700

    convert to blank

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import { createStackNavigator } from 'react-navigation'
 
 import LaunchScreen from './screens/LaunchScreen'
-import Login from './screens/Login'
-import Register from './screens/Register'
-import Dashboard from './screens/Dashboard'
 
 const AppNavigation = createStackNavigator(
   {
     LaunchScreen: {
       screen: LaunchScreen
-    },
-    Login: {
-      screen: Login
-    },
-    Register: {
-      screen: Register
-    },
-    Dashboard: {
-      screen: Dashboard
     }
   },
   {
